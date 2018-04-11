84f1d24e08a41055de3782b972475fa4999682d7
1K56 <1k56@1K56s-MacBook-Pro.local>
2018-04-11 09:00:21 +0300

    adds webpack to dependencies and changes the frontend file structure

---
+++ resources/assets/js/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <example-component/>
+  </div>
+</template>
+
+<script>
+import ExampleComponent from './components/ExampleComponent'
+export default {
+  components: {
+    ExampleComponent
+  }
+}
+</script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,16 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{csrf_token()}}">
         <title>Laravel</title>
     </head>
     <body>
+        <div id="app"></div>
         <form action="/upload" method="post" enctype="multipart/form-data">
             <input type="file" name="media">
             @csrf
             <input type="submit" value="submit">
         </form>
+        <script src="/js/app.js"></script>
     </body>
 </html>
